8a68eae0e5729111705e60fce6bdfb74e4076f83
AFS12 <arthurmbayres@gmail.com>
2021-10-09 18:45:23 -0300

    start

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -4,19 +4,70 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    public int maxHealth = 5;
+    public float timeInvincible = 2.0f;
+
+    public int health { get { return currentHealth; }}
+    int currentHealth;
+    
+    bool isInvincible;
+    float invincibleTimer;
+    
+    public Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
+    public float charSpeed = 3.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // QualitySettings.vSyncCount = 0;
+        // Application.targetFrameRate = 10;
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        Debug.Log(horizontal);
-        Vector2 position = transform.position;
-        position.x = position.x + 0.1f * horizontal;
-        transform.position = position;
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0)
+                isInvincible = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;
+        position.x = position.x + charSpeed * horizontal * Time.deltaTime;
+        position.y = position.y + charSpeed * vertical * Time.deltaTime;
+
+        rigidbody2d.MovePosition(position);
+    }
+
+    public void ChangeHealth(int amount)
+    {
+        if (amount < 0)
+        {
+            if (isInvincible)
+                return;
+            
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
+
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
+    }
+    
+    public void move (){
+        Vector2 position = rigidbody2d.position;
+        position.y = position.y + 2.0f * 1 * Time.deltaTime;
+        rigidbody2d.MovePosition(position);
     }
 }
